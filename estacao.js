55722b7a1b65f6656ad171b3300377eac797bacf
Anderson Lima <andersonlll007@gmail.com>
2022-06-03 22:57:58 -0300

    Pequenos ajustes

--- estacao.js
+++ estacao.js
@@ -1,46 +1,48 @@
-require('dotenv').config()
-const mqtt = require('mqtt')
+require("dotenv").config();
+const mqtt = require("mqtt");
 
-const lixeiras = []
+const lixeiras = [];
 
-var options = {
-    host: process.env.HOST,
-    port: process.env.PORTA,
-    protocol: process.env.PROTOCOLO,
-    username: process.env.USUARIO,
-    password: process.env.SENHA,
-}
+let options = {
+  host: process.env.HOST,
+  port: process.env.PORTA,
+  protocol: process.env.PROTOCOLO,
+  username: process.env.USUARIO,
+  password: process.env.SENHA,
+};
 
-
-/** Estacao A 
+/** Estacao A
  * Se inscreve no tópico */
-const cliente = mqtt.connect(options)
-const topico = 'estacaoA/#';
-cliente.on('connect', () => {
-    console.log('Conectado ao Tópico EstacaoA')
-    cliente.subscribe([topico], () => {
-      console.log(`Estacao inscrita no tópico: '${topico}'`)
-    })
-  })
+const cliente = mqtt.connect(options);
+const topico = "estacaoA/#";
+cliente.on("connect", () => {
+  console.log("Conectado ao Tópico EstacaoA");
+  cliente.subscribe([topico], () => {
+    console.log(`Estacao inscrita no tópico: '${topico}'`);
+  });
+});
 
-function atualizarLixeira(valores){
+function atualizarLixeira(valores) {
   encontrado = false;
-  lixeiras.forEach(function(lixeira, i){
-    if(lixeira.codigo == valores.codigo){
-      lixeira.ocupacaoAtual = valores.ocupacaoAtual
-      encontrado = true
-      return true
+  lixeiras.forEach(function (lixeira, i) {
+    if (lixeira.codigo == valores.codigo) {
+      lixeira.ocupacaoAtual = valores.ocupacaoAtual;
+      encontrado = true;
+      return true;
     }
-  })
+  });
 
-  if(!encontrado){    
-    lixeiras.push({"codigo": valores.codigo, "ocupacaoAtual": valores.ocupacaoAtual});    
+  if (!encontrado) {
+    lixeiras.push({
+      codigo: valores.codigo,
+      ocupacaoAtual: valores.ocupacaoAtual,
+    });
   }
-  console.log(lixeiras)
+  console.log(lixeiras);
 }
 
 /** Alteracao no topico ou subtopicos */
-cliente.on('message', (topico, payload) => {  
-  dados = JSON.parse(payload.toString());   
-  atualizarLixeira(dados)
-})
+cliente.on("message", (topico, payload) => {
+  dados = JSON.parse(payload.toString());
+  atualizarLixeira(dados);
+});
--- lixeiraVF.js
+++ lixeiraVF.js
@@ -1,73 +1,79 @@
-require('dotenv').config()
-const mqtt = require('mqtt')
+require("dotenv").config();
+const mqtt = require("mqtt");
 
 const lixeira = {
-    codigo: 2,
-    longitude: 0.0,
-    latitude: 0.0,
-    capacidadeAtual: 0.0,
-    capacidadeMaxima: 100.0,
-    ocupacaoAtual: 0.0, // dado em porcentagem (%)
-    estacao: 'B',
-}
+  codigo: 2,
+  longitude: 0.0,
+  latitude: 0.0,
+  capacidadeAtual: 0.0,
+  capacidadeMaxima: 100.0,
+  ocupacaoAtual: 0.0, // dado em porcentagem (%)
+  estacao: "B",
+};
 
-var configuracaoRecebimento = {
-    host: process.env.HOST,
-    port: process.env.PORTA,
-    protocol: process.env.PROTOCOLO,
-    username: process.env.USUARIO,
-    password: process.env.SENHA,
-}
+let configuracaoRecebimento = {
+  host: process.env.HOST,
+  port: process.env.PORTA,
+  protocol: process.env.PROTOCOLO,
+  username: process.env.USUARIO,
+  password: process.env.SENHA,
+};
 
 /** Se inscreve no tópico */
-const cliente = mqtt.connect(configuracaoRecebimento)
-const topico = 'estacaoB/esvaziar_lixeira'
-cliente.on('connect', () => {
-    console.log('Conectado ao Tópico EstacaoA')
-    cliente.subscribe([topico], () => {
-      console.log(`Lixeira inscrita no tópico: '${topico}'`)
-    })
-  })
+const cliente = mqtt.connect(configuracaoRecebimento);
+const topico = "estacaoB/esvaziar_lixeira";
+cliente.on("connect", () => {
+  console.log("Conectado ao Tópico EstacaoA");
+  cliente.subscribe([topico], () => {
+    console.log(`Lixeira inscrita no tópico: '${topico}'`);
+  });
+});
 
 /**Enviar dados */
-function enviarDados(){                        
-        topicoLixeira = 'estacaoB/Lixeira'+lixeira.codigo
-        console.log(topicoLixeira)
-        console.log(lixeira.ocupacaoAtual)
-        lixeiraString = JSON.stringify(lixeira)
-        cliente.publish(topicoLixeira, lixeiraString, { qos: 0, retain: false }, (error) => {
-            if (error) {
-            console.error(error)
-            }
-        })        
-        console.log("publicou a ocupacaoAtual tal:" + lixeira.ocupacaoAtual)        
+function enviarDados() {
+  topicoLixeira = "estacaoB/Lixeira" + lixeira.codigo;
+  console.log(topicoLixeira);
+  console.log(lixeira.ocupacaoAtual);
+  lixeiraString = JSON.stringify(lixeira);
+  cliente.publish(
+    topicoLixeira,
+    lixeiraString,
+    { qos: 0, retain: false },
+    (error) => {
+      if (error) {
+        console.error(error);
       }
+    }
+  );
+  console.log("Publicou a ocupacaoAtual tal:" + lixeira.ocupacaoAtual);
+}
 
 /** Recebe dados do topico inscrito*/
-cliente.on('message', (topic, payload) => {
-    dados = JSON.parse(payload.toString());           
-    if(dados.codigo == lixeira.codigo){
-        lixeira.capacidadeAtual = 0
-        lixeira.ocupacaoAtual = 0
-        enviarDados()
-        console.log("Lixeira " + lixeira.codigo + " esvaziada!!")
-    }    
-  })
-
-  function adicionarLixo(quantidade) {
-         novaCapacidadeAtual = lixeira.capacidadeAtual + quantidade;
-         if (novaCapacidadeAtual > lixeira.capacidadeMaxima)
-            console.log("A quantidade ultrapassa a capacidade maxima da lixeira")
-         else {
-             lixeira.capacidadeAtual = novaCapacidadeAtual;
-             lixeira.ocupacaoAtual = (lixeira.capacidadeAtual / lixeira.capacidadeMaxima) * 100;
-             enviarDados();
-         }   
+cliente.on("message", (topic, payload) => {
+  dados = JSON.parse(payload.toString());
+  if (dados.codigo == lixeira.codigo) {
+    lixeira.capacidadeAtual = 0;
+    lixeira.ocupacaoAtual = 0;
+    enviarDados();
+    console.log(`Lixeira ${lixeira.codigo} esvaziada!`);
   }
+});
 
-  /** Automatização */
-  setInterval(() => {    
-    quantidade = Math.random() * 100.0;
-    console.log("Adicionando " + quantidade + " em lixo...");
-    adicionarLixo(quantidade);
-  }, 5000);
+function adicionarLixo(quantidade) {
+  novaCapacidadeAtual = lixeira.capacidadeAtual + quantidade;
+  if (novaCapacidadeAtual > lixeira.capacidadeMaxima)
+    console.log("A quantidade ultrapassa a capacidade maxima da lixeira");
+  else {
+    lixeira.capacidadeAtual = novaCapacidadeAtual;
+    lixeira.ocupacaoAtual =
+      (lixeira.capacidadeAtual / lixeira.capacidadeMaxima) * 100;
+    enviarDados();
+  }
+}
+
+/** Automatização - a cada 5s uma quantidade de lixo é adicionada*/
+setInterval(() => {
+  quantidade = Math.random() * 100.0;
+  console.log(`Adicionando ${quantidade} m³ de lixo...`);
+  adicionarLixo(quantidade);
+}, 5000);
